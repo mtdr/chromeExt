f37c0cff5a03334e37ec786144c5e13399322e08
mtdr <paul.osmolovsky@gmail.com>
2018-09-16 23:12:20 +0300

    0.2 Скрипт обновлен, нужно подобрать правильное событие для обработки

--- inject.js
+++ inject.js
@@ -1,16 +1,21 @@
 'use strict';
 
-document.addEventListener('click', function () {
-    var buttonOnSite = document.querySelector(".comment-widget__header"),
-        parent = buttonOnSite.parentElement,
-        next = buttonOnSite.nextSibling,
-        button = document.createElement("button"),
-        text = document.createTextNode("test");
 
-    button.appendChild(text);
-    if (next) {
-        parent.insertBefore(button, next);
-    } else {
-        parent.appendChild(button);
-    }
+document.addEventListener('click', function () {
+    let commentHeader = document.getElementsByClassName("comment-widget__header");
+
+    Array.from(commentHeader).forEach(function (item, i, commentHeader) {
+        let parent = item.parentElement,
+            next = item.nextSibling,
+            button = document.createElement("button"),
+            text = document.createTextNode("test");
+
+        button.appendChild(text);
+        if (next) {
+            parent.insertBefore(button, next);
+        } else {
+            parent.appendChild(button);
+        }
+    });
 });
+// <button class="discussions__load-btn small white expand" data-ember-action="1607">Показать обсуждения (1)</button>
